cba5fe6707938c3ab78332068c07c671cd3a89dc
Eric P <pcderic@gmail.com>
2019-06-15 19:27:56 -0400

    Make drink parameters adjustable

--- public/goeasy/scripts/goeasy.js
+++ public/goeasy/scripts/goeasy.js
@@ -1,23 +1,88 @@
-let weeklyUnits = 14;
+// Constants
+const weeklyUnits = 14;
 
+// Default values
+let defaults = [
+    [40, 25, "Scotch"],
+    [12, 125, "WineByVolume"],
+    [12, 175, "WineGlass"],
+    [5, 330, "BeerBottle"],
+    [5, 500, "BeerPint"],
+    [3.5, 341, "Custom"]
+];
+
+// Reset drinking units and log every week
 function resetUnits() {
-    let ts = new Date();
     let unitsLeft = weeklyUnits;
+
+    // Reset drink log
+    localStorage.setItem("log", "");
+    logger("Reset");
+
+    // Reset units left
     $("#unitsLeft").val(unitsLeft);
     localStorage.setItem("unitsLeft", unitsLeft);
-    $("#inVolume").val(localStorage.getItem("inVolume"));
-    $("#inStrength").val(localStorage.getItem("inStrength"));
-    $("#customUnits").val(getCustomUnits());
-    localStorage.setItem("log", "");
-    logger(ts.toLocaleString() + ": Reset unit(s)");
+
     return unitsLeft;
 }
 
-function takeUnits(unitsTaken, drinkType) {
-    let ts = new Date();
-    logger(ts.toLocaleString() + ": Took " + unitsTaken + " " + drinkType + " un.");
+// Restore tweaked strength/volume drink values from local storage and update UI
+function restoreDrinkParams() {
+    for (let i = 0; i < defaults.length; i++) {
+        // Get default values
+        let defaultStrength = defaults[i][0];
+        let defaultVolume = defaults[i][1];
+        let defaultName = defaults[i][2];
+
+        // Get saved values
+        let strength = localStorage.getItem(defaultName + "Strength");
+        let volume = localStorage.getItem(defaultName + "Volume");
+        let name = localStorage.getItem(defaultName + "Name");
+
+        // Use default values if nothing was saved before
+        if (!strength) strength = defaultStrength;
+        if (!volume) volume = defaultVolume;
+        if (!name) name = defaultName;
+
+        // Calculate Units
+        let units = Number((strength * volume) / 1000).toPrecision(2);
+
+        // Update UI
+        $("#" + defaultName + "Strength").val(strength);
+        $("#" + defaultName + "Volume").val(volume);
+        $("#" + defaultName + "Name").val(name);
+        $("#" + defaultName + "Units").val(units);
+    }
+    saveDrinkParams();
+}
+
+// Save tweaked strength/volume drink values from UI
+function saveDrinkParams() {
+    for (let i = 0; i < defaults.length; i++) {
+        let drink = defaults[i][2];
+
+        // Get values from UI
+        let strength = $("#" + drink + "Strength").val();
+        let volume = $("#" + drink + "Volume").val();
+        let name = $("#" + drink + "Name").val();
+
+        // Calculate units and update UI
+        let units = Number((strength * volume) / 1000).toPrecision(2);
+        $("#" + drink + "Units").val(units);
+
+        // Save values
+        localStorage.setItem(drink + "Strength", strength);
+        localStorage.setItem(drink + "Volume", volume);
+        localStorage.setItem(drink + "Name", name);
+    }
+}
+
+function takeUnits(drinkType) {
+    let unitsTaken = $("#" + drinkType + "Units").val();
+    logger(unitsTaken + " " + drinkType + " un.");
     let unitsLeft = $("#unitsLeft").val();
-    unitsLeft -= Number(unitsTaken).toPrecision(2);
+    unitsLeft -= unitsTaken;
+    unitsLeft = Number(unitsLeft).toPrecision(2);
     $("#unitsLeft").val(unitsLeft);
     localStorage.setItem("unitsLeft", unitsLeft);
     saveCustoms();
@@ -31,30 +96,32 @@ function restoreUnits() {
 }
 
 function saveCustoms() {
-    localStorage.setItem("inVolume", $("#inVolume").val());
-    localStorage.setItem("inStrength", $("#inStrength").val());
+    localStorage.setItem("CustomVolume", $("#CustomVolume").val());
+    localStorage.setItem("CustomStrength", $("#CustomStrength").val());
 }
 
 function restoreCustoms() {
-    $("#inVolume").val(localStorage.getItem("inVolume"));
-    $("#inStrength").val(localStorage.getItem("inStrength"));
+    $("#CustomVolume").val(localStorage.getItem("CustomVolume"));
+    $("#CustomStrength").val(localStorage.getItem("CustomStrength"));
     $("#customUnits").val(getCustomUnits());
 }
 
 function getCustomUnits() {
-    let inStrength = $("#inStrength").val();
-    let inVolume = $("#inVolume").val();
-    let customUnits = Number((inStrength * inVolume) / 1000).toPrecision(2);
+    let customStrength = $("#CustomStrength").val();
+    let customVolume = $("#CustomVolume").val();
+    let customUnits = Number((customStrength * customVolume) / 1000).toPrecision(2);
     $("#customUnits").val(customUnits);
     saveCustoms();
     return customUnits;
 }
 
 function logger(input) {
+    let ts = new Date();
+    let msg = ts.toLocaleDateString() + ": " + input;
     let entries = localStorage.getItem("log");
-    if (entries === null) localStorage.setItem("log", input);
-    else localStorage.setItem("log", entries + "\n" + input);
-    console.log(input);
+    if (entries === null) localStorage.setItem("log", msg);
+    else localStorage.setItem("log", entries + "\n" + msg);
+    console.log(msg);
 }
 
 function showLog() {
